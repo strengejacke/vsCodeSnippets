3c4bfa5b332460bad1a33fe4174855f8085e9dd9
Daniel <mail@danielluedecke.de>
2022-08-09 11:19:34 +0200

    add unload_namespace arg

--- R/update_R_packages.r
+++ R/update_R_packages.r
@@ -1,5 +1,20 @@
 #' @export
-update_R_packages <- function() {
+update_R_packages <- function(unload_namespace = TRUE) {
+  if (isTRUE(unload_namespace)) {
+    loaded_pkgs <- loadedNamespaces()
+    base_pkgs <- c(
+      "base", "compiler", "datasets", "graphics", "grDevices", "grid",
+      "methods", "parallel", "splines", "stats", "stats4", "tools", "tcltk",
+      "utils"
+    )
+    needs_unload <- setdiff(loaded_pkgs, base_pkgs)
+    if (length(needs_unload)) {
+      for (p in needs_unload) {
+        tryCatch(unloadNamespace(p), error = function(e) NULL)
+      }
+    }
+  }
+
   old_packages <- old.packages()
   user_packages <- grepl(
     paste0("^", dirname(path.expand("~"))),
