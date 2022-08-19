b9cf26eaa589abe2fd070cb3437a977a4a0a240f
Daniel <mail@danielluedecke.de>
2022-08-19 09:17:59 +0200

    add debug-fun

---
+++ R/load_debug_pkg.r
@@ -0,0 +1,9 @@
+# This functions loads the current package and refreshed break-points in
+# VS Code. Use this to debug packages with breakpoint
+
+#' @export 
+load_debug_pkg <- function() {
+  ret <- pkgload::load_all()
+  exports <- as.environment(paste0("package:", environmentName(ret$env)))
+  vscDebugger::.vsc.refreshBreakpoints(list(ret$env, exports))
+}
