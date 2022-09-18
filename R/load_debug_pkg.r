4a4404f90a0a8afd1a103ef9b2eef78067bb83f7
Daniel <mail@danielluedecke.de>
2022-09-18 11:39:51 +0200

    skip pkgs that can't be unloaded

--- R/load_debug_pkg.r
+++ R/load_debug_pkg.r
@@ -1,7 +1,7 @@
 # This functions loads the current package and refreshed break-points in
 # VS Code. Use this to debug packages with breakpoint
 
-#' @export 
+#' @export
 load_debug_pkg <- function() {
   ret <- pkgload::load_all()
   exports <- as.environment(paste0("package:", environmentName(ret$env)))
--- R/update_R_packages.r
+++ R/update_R_packages.r
@@ -8,9 +8,14 @@ update_R_packages <- function(unload_namespace = TRUE) {
       "utils"
     )
     needs_unload <- setdiff(loaded_pkgs, base_pkgs)
+    could_not_unload <- c()
     if (length(needs_unload)) {
       for (p in needs_unload) {
-        tryCatch(unloadNamespace(p), error = function(e) NULL)
+        tryCatch(unloadNamespace(p),
+                 error = function(e) {
+                   # remember packages that could not be unloaded.
+                   could_not_unload <- c(could_not_unload, p)
+                 })
       }
     }
   }
@@ -23,13 +28,27 @@ update_R_packages <- function(unload_namespace = TRUE) {
 
   if (any(user_packages)) {
     needs_update <- names(old_packages[, "LibPath"][user_packages])
-    msg <- paste0(
-      "\nInstalling ", length(needs_update), " package",
-      ifelse(length(needs_update) > 1, "s", ""), ": ",
-      datawizard::text_concatenate(needs_update), "\n\n"
-    )
-    insight::print_color(msg, "blue")
-    utils::install.packages(needs_update)
+    # skip packages that could not be unloaded...
+    if (length(could_not_unload)) {
+      msg <- paste0(
+        "\nFollowing package", ifelse(length(could_not_unload) > 1, "s have", "has"),
+        " updates but could not be unloaded:",
+        datawizard::text_concatenate(could_not_unload)
+      )
+      insight::print_color(insight::format_message(
+        msg, "Please try installing them using pure R.\n\n"
+      ), "red")
+      needs_update <- setdiff(needs_update, could_not_unload)
+    }
+    if (length(needs_update)) {
+      msg <- paste0(
+        "\nInstalling ", length(needs_update), " package",
+        ifelse(length(needs_update) > 1, "s", ""), ": ",
+        datawizard::text_concatenate(needs_update), "\n\n"
+      )
+      insight::print_color(msg, "blue")
+      utils::install.packages(needs_update)
+    }
   } else {
     insight::print_color("\nAll packages are up to date!\n\n", "green")
   }
